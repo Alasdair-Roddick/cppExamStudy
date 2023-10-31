b18fce2c7fe5f3aaf7ddf06e50e4508c8032c17a
Alasdair-Roddick <64404140+Alasdair-Roddick@users.noreply.github.com>
2023-10-31 20:41:44 +1030

    Userinput done

---
+++ User-Input-Lesson-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x; 
+    cout << "Enter a number: "; // 5
+    cin >> x; // 5
+    cout << "You entered " << x << endl; 
+}
+
+
+// Output: You entered 5
---
+++ User-Input-Lesson-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << "Enter another number: ";
+    cin >> y;
+    int sum = x + y;
+    cout << "The sum of " << x << " and " << y << " is " << sum << endl;
+
+    return 0;
+}
+
